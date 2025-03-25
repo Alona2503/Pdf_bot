608233410638dc3ff7926506df92de8cdfb686b6
Alona2503 <alena.mazur1989@gmail.com>
2025-03-25 13:01:18 +0200

    відступ

--- bot.py
+++ bot.py
@@ -499,7 +499,7 @@ def handle_response(update: Update, context: CallbackContext):
     elif state == "note_entry":
             # Розбиваємо текст на рядки перед збереженням
                 lines = text.split('\n')
-                    add_entry(user_id, "note", lines)
+                add_entry(user_id, "note", lines)
         context.user_data["state"] = None
         update.message.reply_text("✅ Нотатка збережена!")
 
